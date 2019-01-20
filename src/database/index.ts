4f37031445ce4ac55eee7ecced552f6d09d9fce7
enigma <monstawoodwow@gmail.com>
2019-01-20 12:23:04 +0000

    Exported more interfaces

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,3 +3,5 @@
  */
 export { MysqlAdapter } from "./adapters/mysql";
 export { PostgresAdapter } from './adapters/postgres'
+export { IConnectionFunction, IMysqlObjectLiteral } from './adapters/interfaces/sql'
+export { transformSet, bindMarks } from './adapters/util'
