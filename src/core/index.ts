d604c049ed15ce159cf802847bcee09332e864a2
enigma <monstawoodwow@gmail.com>
2019-01-20 19:14:35 +0000

    Updated some exports

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,3 +1,6 @@
 export * from './neo-app'
 export * from './interfaces'
 export * from './exceptions'
+export { ModelRepository } from './model-repository'
+export { Repository } from './repository'
+export { RepositoryFactory } from './factory'
