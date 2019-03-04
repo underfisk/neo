5c9292c54ae6270c69e55f86cf9ae15dd90dbf7b
Rodrigo <monstawoodwow@gmail.com>
2019-03-04 17:52:38 +0000

    Added more exports

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,15 @@
  * @package Neo.ts
  * @author Enigma
  * @namespace Neo
+ * @updated March 3
  * @link https://github.com/underfisk/neo
  * 
  */
 export * from './common'
-export * from './core'
+export {
+  NeoApplication,
+  ModelRepository,
+  Repository,
+  RepositoryFactory  
+} from './core'
 export * from './database'
